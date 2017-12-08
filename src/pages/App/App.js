1c38a42516407163ba590d49771bd2b1bb498cc4
Griffin Scott <griffinoscott@gmail.com>
2017-12-08 11:28:27 -0800

    added pages

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,18 +1,27 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom'
 import logo from '../../logo.svg';
 import './App.css';
+import CataloguePage from '../Catalogue/CataloguePage'
+import CheckoutPage from '../Checkout/CheckoutPage'
+import ConfirmationPage from '../Confirmation/ConfirmationPage'
+import WelcomePage from '../Welcome/WelcomePage'
+
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+    return(
+      <div>
+        <h1 className="App-title">Amazio</h1>
+        <CataloguePage />
+        <CheckoutPage />
+        <ConfirmationPage />
+        <WelcomePage />
+
       </div>
     );
   }
---
+++ src/pages/Catalogue/CataloguePage.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const CataloguePage = () => {
+  return (
+    <h1>Catalogue Page</h1>
+  )
+}
+
+export default CataloguePage;
